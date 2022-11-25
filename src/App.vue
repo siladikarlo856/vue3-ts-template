8a670a3597e7dabfb929628e68292a63744558e2
Karlo Siladi <karlo.siladi@DPMDevOps.onmicrosoft.com>
2022-11-25 22:59:51 +0100

    Pinia store added

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,28 @@
     <div>Language: TypeScript</div>
     <div>CSS: Tailwind</div>
   </div>
+  <div class="mt-10 text-3xl mx-auto max-w-6xl">
+    Counter: {{ counterStore.count }}
+    <button
+      @click="counterStore.increment"
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Increment
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useCounterStore } from "./stores/counterStore";
 
 export default defineComponent({
   setup() {
+    const counterStore = useCounterStore();
+
     console.log("Test lint warning");
 
-    return {};
+    return { counterStore };
   },
 });
 </script>
