6ae64a925cec9b9a7bac430606e3638a629d05cc
Karlo Siladi <karlo.siladi@DPMDevOps.onmicrosoft.com>
2022-11-26 00:13:24 +0100

    devextreme added

--- src/bootloader.ts
+++ src/bootloader.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
+import "devextreme/dist/css/dx.light.css";
 import "./index.scss";
 
 import App from "./App.vue";
--- src/views/HomeView/HomeView.vue
+++ src/views/HomeView/HomeView.vue
@@ -1,25 +1,35 @@
 <template>
-  <div class="mt-10 text-3xl mx-auto max-w-6xl">
-    Counter: {{ counterStore.count }}
-    <button
-      @click="counterStore.increment"
-      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-    >
-      Increment
-    </button>
+  <div class="flex flex-col mx-auto">
+    <div class="mt-10 text-3xl mx-auto max-w-6xl">
+      Counter: {{ counterStore.count }}
+      <button
+        @click="counterStore.increment"
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Increment
+      </button>
+    </div>
+    <div class="mt-10 mx-auto">
+      <DxButton text="Click me" @click="sayHelloWorld" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import DxButton from "devextreme-vue/button";
 import { useCounterStore } from "@/stores/counterStore";
 
 export default defineComponent({
   name: "HomeView",
+  components: { DxButton },
   setup() {
     const counterStore = useCounterStore();
 
-    return { counterStore };
+    function sayHelloWorld() {
+      alert("Hello world!");
+    }
+    return { counterStore, sayHelloWorld };
   },
 });
 </script>
